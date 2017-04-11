efa895a6b594918e042c5081cc7c6838076c9acb
lvyeRose <lvyeRose@163.com>
2017-04-11 10:36:10 +0800

    base map show

--- app/src/main/java/com/app/lvyerose/amap/amaprose/MainActivity.java
+++ app/src/main/java/com/app/lvyerose/amap/amaprose/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 import com.app.lvyerose.amap.amaprose.map.location.LocationBuilder;
 import com.app.lvyerose.amap.amaprose.map.location.LocationUtils;
 import com.app.lvyerose.amap.amaprose.map.location.PartLocationListener;
@@ -13,17 +15,25 @@ public class MainActivity extends AppCompatActivity {
     private LocationUtils locationUtils;
 
     TextView locationShowTv;
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        locationShowTv = (TextView) findViewById(R.id.location_tv);
-        start();
-
+        initView();
+        startLocation();
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        initShowMap();
     }
 
-    public void start() {
+    private void initView() {
+        locationShowTv = (TextView) findViewById(R.id.location_result_tv);
+        mapView = (MapView) findViewById(R.id.main_map_view);
+    }
+
+    //开始定位
+    public void startLocation() {
         locationUtils = new LocationBuilder()
                 .setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy)
                 .setNeedAddress(true)
@@ -58,6 +68,12 @@ public class MainActivity extends AppCompatActivity {
         locationUtils.start();
     }
 
+    public void initShowMap() {
+        AMap aMap = mapView.getMap();
+        aMap.setTrafficEnabled(true);// 显示实时交通状况
+        //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
+        aMap.setMapType(AMap.MAP_TYPE_BUS);// 卫星地图模式
+    }
 
     @Override
     protected void onDestroy() {
